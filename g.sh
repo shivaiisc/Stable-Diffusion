07351ce714b1f8d87aa5ed00b75a27c64f4aa28b
shivaiisc <shivachandhraa@gmail.com>
2024-09-22 13:49:34 +0530

    first-commit

--- g.sh
+++ g.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m 'first-commit'
-git branch -M main
-git remote add origin git@github.com:shivaiisc/Stable-Diffusion.git
+# git branch -M main
+# git remote add origin git@github.com:shivaiisc/Stable-Diffusion.git
 
 git push -u origin main
